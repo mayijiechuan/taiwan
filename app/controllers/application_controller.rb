14f5c7d3669e67d1331499b34f744b4e03806396
mai minagawa <mai.minapark@gmail.com>
2015-12-18 10:37:57 +0000

    twoone

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,10 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_action :set_years
+  private
+  def set_years
+    @years = Idea.pluck(:created_at).map{|created_at| created_at.year }.uniq
+  end
+
 end
